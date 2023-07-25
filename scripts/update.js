5487f3c59cec4bb036ed2d7d4f46a6ff8a5743da
CENSOR_1337 <viixxivvimm@gmail.com>
2023-07-25 16:48:04 +0700

    fix: create folder if not exist

--- scripts/update.js
+++ scripts/update.js
@@ -1,4 +1,4 @@
-import fs from "fs";
+const fs = require("fs");
 
 const nativeDbLink = "https://natives.altv.mp/natives";
 
@@ -6,6 +6,9 @@ async function downloadJsonNative() {
     const response = await fetch(nativeDbLink);
     const data = await response.json();
     const jsonData = JSON.stringify(data, null, 4);
+    if (!fs.existsSync("./bin")) {
+        fs.mkdirSync("./bin");
+    }
     fs.writeFile(`./bin/natives.json`, jsonData, (err) => {
         if (err) {
             console.error(err);
